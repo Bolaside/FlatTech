7a4a166aed1b6f3e43d1908523cc5bebb3aa1adc
Bolaside <bolaside@gmail.com>
2025-05-20 11:53:33 -0400

    scripty

--- update.sh
+++ update.sh
@@ -2,10 +2,13 @@
 # this is only meant for me cause idk
 # code -> game
 for folder in config defaultconfigs kubejs; do
-  cp -rf $folder/ $MODPACKS/flatTech/minecraft/
+  cp -rf $folder $MODPACKS/flatTech/minecraft
 done
 
 # game -> code
 for folder in .probe .vscode; do
   cp -rf $MODPACKS/flatTech/minecraft/$folder .
 done
+
+# copy configs that don't exist over
+cp -r --update=none $MODPACKS/flatTech/minecraft/config/* ./config/
