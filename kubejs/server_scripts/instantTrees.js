b24bf3ace913f99c274f2e3807877a8a8b5e8cf5
Bolaside <bolaside@gmail.com>
2025-06-24 22:05:03 +0300

    refactor: enforce 100 line width

--- kubejs/server_scripts/instantTrees.js
+++ kubejs/server_scripts/instantTrees.js
@@ -6,7 +6,12 @@ BlockEvents.rightClicked(event => {
     const { x, y, z } = block.pos
     const pos = [x, y, z].join(" ")
 
-    block.blockState.block.advanceTree(event.level, block.pos, block.blockState, event.level.random)
+    block.blockState.block.advanceTree(
+      event.level,
+      block.pos,
+      block.blockState,
+      event.level.random
+    )
     // summon 32 composter particles at the block in a 1.6^3 cube
     // cause apparently the radius is multiplied by 8
     event.server.runCommandSilent(`particle composter ${pos} 0.2 0.2 0.2 0.5 32 normal @a`)
