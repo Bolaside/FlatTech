e44ee4e40c155b253e78538498391f25892acf3a
Bolaside <bolaside@gmail.com>
2025-06-30 00:32:05 +0300

    refactor: :art: move the repo to the parent directory
    holy shit that's a lot of changes
    
    =D

--- kubejs/server_scripts/motd.js
+++
@@ -1,3 +0,0 @@
-PlayerEvents.loggedIn(event => {
-  event.player.tell(`Welcome to ${Text.red("FlatTech Alpha")}. This is a test version.`)
-})
--- kubejs/server_scripts/starterItems.js
+++
@@ -1,9 +0,0 @@
-PlayerEvents.loggedIn(event => {
-  const STAGE_NAME = "receivedStarterItems"
-  const { player } = event
-
-  if (!player.stages.has(STAGE_NAME)) {
-    player.stages.add(STAGE_NAME)
-    player.give(Item.of("tempad:he_who_remains_tempad"))
-  }
-})
